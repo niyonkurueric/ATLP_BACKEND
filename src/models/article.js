fd8053be9fc2abb4dd7c560399f8f3af9582f3ec
niyonkurueric <niyonkurufamous@gmail.com>
2022-01-28 17:01:44 +0200

    queries

--- src/models/article.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const articleSchema = mongoose.Schema({
-    title: { type: String, required: true },
-    photo: { type: String },
-    content: { type: String, required: true },
-    likes: { type: Number, default: 0 }
-})
-const Article = mongoose.model('Article', articleSchema)
-export default Article
