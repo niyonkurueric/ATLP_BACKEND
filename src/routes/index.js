9eebc85ac62e2a4282a80a7cabd026aafb780c37
niyonkurueric <niyonkurufamous@gmail.com>
2022-02-02 15:36:11 +0200

    comment user authentication

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import articleRoutes from "./api/articleRoutes"
 import queriesRoutes from "./api/queriesRoutes"
 import commentRoutes from "./api/commentRoutes"
 import userRoutes from "./api/userRoutes"
-
+// npm install chai chai-http coveralls mocha nyc sinon 
 const routes = express.Router()
 
 routes.use('/', welcomeRoutes)
